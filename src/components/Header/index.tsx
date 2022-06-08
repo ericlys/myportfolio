3a077a72e4f5f06c1ca8ba006ecfb82f01584e25
Ericlys Moreira <ericlysm@gmail.com>
2022-06-07 22:42:52 -0300

    feat: adding more pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { ActiveLink } from '../ActiveLink';
 import styles from './styles.module.scss';
 
 export function Header(){
@@ -6,10 +7,18 @@ export function Header(){
       <div className={styles.headerContent}>
        <h2><strong>@</strong>EriclysMoreira</h2>
         <nav>
+        <ActiveLink activeClassName={styles.active} href="/">
           <a href='#' className={styles.active}>About me</a>
+        </ActiveLink>
+        <ActiveLink activeClassName={styles.active} href="/skills">
           <a href='#'>Skills</a>
+        </ActiveLink>
+        <ActiveLink activeClassName={styles.active} href="/project">
           <a href='#'>Project</a>
+        </ActiveLink>
+        <ActiveLink activeClassName={styles.active} href="/contact">
           <a href='#'>Contact</a>
+        </ActiveLink>
         </nav>
       </div>
     </header>
