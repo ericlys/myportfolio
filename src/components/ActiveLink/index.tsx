975be49e640f0f2c2fc09bc1da04372e84c5290e
Ericlys Moreira <ericlysm@gmail.com>
2022-06-28 23:58:09 -0300

    adding mobile menu

--- src/components/ActiveLink/index.tsx
+++ src/components/ActiveLink/index.tsx
@@ -1,10 +1,14 @@
 import Link, { LinkProps } from "next/link";
 import { useRouter } from "next/router";
+import { IconProps } from "phosphor-react";
 import { cloneElement, ReactElement } from "react";
+
+// type IconType = (props: IconProps) => JSX.Element;
 
 interface ActiveLinkProps extends LinkProps {
   children: ReactElement;
   activeClassName: string;
+  // icon?: IconType;
 }
 
 export function ActiveLink({children, activeClassName, ...rest}: ActiveLinkProps) {
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { ActiveLink } from '../ActiveLink';
-import styles from './styles.module.scss';
+import styles from './header.module.scss';
+import { House, ListChecks, PaperPlaneTilt, Wrench } from 'phosphor-react';
 
 export function Header(){
   return(
@@ -7,17 +8,29 @@ export function Header(){
       <div className={styles.headerContent}>
        <h2><strong>@</strong>EriclysMoreira</h2>
         <nav>
-        <ActiveLink activeClassName={styles.active} href="/">
-          <a href='#' className={styles.active}>About me</a>
+        <ActiveLink activeClassName={styles.active} href="/">         
+          <a href='#' className={styles.active}>
+            <House size={32}/>
+            <span>About me</span>
+          </a>
         </ActiveLink>
         <ActiveLink activeClassName={styles.active} href="/skills">
-          <a href='#'>Skills</a>
+          <a href='#'>
+            <Wrench size={32} />
+            <span>Skills</span>
+          </a>
         </ActiveLink>
         <ActiveLink activeClassName={styles.active} href="/project">
-          <a href='#'>Project</a>
+          <a href='#'>
+            <ListChecks size={32} />
+            <span>Project</span>
+          </a>
         </ActiveLink>
         <ActiveLink activeClassName={styles.active} href="/contact">
-          <a href='#'>Contact</a>
+          <a href='#'>
+            <PaperPlaneTilt size={32} /> 
+            <span>Contact</span>
+          </a>
         </ActiveLink>
         </nav>
       </div>
